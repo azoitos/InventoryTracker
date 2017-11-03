70a12474d1304e8e238bb582a24e84e8be18c662
Alexander Zoitos <azoitos1@gmail.com>
2017-11-03 13:26:20 -0400

    Fix: Login Re-render after sign-in

--- app/store.jsx
+++ app/store.jsx
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index.jsx';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { createLogger } from 'redux-logger';
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        createLogger()
-    )
+    composeWithDevTools(
+        applyMiddleware(
+            thunkMiddleware,
+            createLogger()
+        ))
 );
 
 export default store;
