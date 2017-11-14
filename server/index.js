fea7272c4adcf07d368a5c2ec278c1e4d5ad0394
David Ko <dk577@cornell.edu>
2017-11-14 12:41:44 -0500

    routespart1

--- server/index.js
+++ server/index.js
@@ -83,7 +83,7 @@ app.get('*', (req, res) => {
 //start up server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    db.sync({})
+    db.sync({force: true})
     console.log(`Welcome! You are now listening on port ${port}`);
 });
 
