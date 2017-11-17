428e0580e33846dae0e2e590cc8f607e822335a3
Alexander Zoitos <azoitos1@gmail.com>
2017-11-17 12:03:55 -0500

    Add: Update Product Form

--- app/components/SingleProduct.jsx
+++ app/components/SingleProduct.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap'
+import { Panel, TextInput } from 'react-bootstrap'
 import { connect } from 'react-redux';
 
-import { getSingleProduct, deleteProduct } from '../reducers/products.jsx'
+import { getSingleProduct, deleteProduct, updateProduct } from '../reducers/products.jsx'
 
 export class SingleProduct extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: this.props.product.description,
+            quantity: this.props.product.quantity,
+            price: this.props.product.price
+        }
+        this.updateSearch = this.updateSearch.bind(this);
+        // this.renderUpdateProductForm = this.renderUpdateProductForm.bind(this);
+    }
 
     componentDidMount() {
         this.props.getSingleProduct(this.props.match.params.productId)
+    }
+
+    updateSearch(event) {
+        this.setState({
+            description: event.target.value,
+            quantity: event.target.value,
+            price: event.target.value
+        })
+    }
+
+    onSubmit(event) {
+        const singleProduct = this.props.product
+        event.preventDefault();
+        const updatedProduct = {
+            description: event.target.description.value || singleProduct.description,
+            quantity: event.target.quantity.value || singleProduct.quantity,
+            price: event.target.price.value || singleProduct.price
+        }
+        this.props.updateProduct(singleProduct.productId, updatedProduct);
+        event.target.description.value = '';
+        event.target.quantity.value = '';
+        event.target.price.value = '';
     }
 
     render() {
@@ -39,15 +71,46 @@ export class SingleProduct extends Component {
                 :
                 <div>Loading Single Product</div>
         )
+    }
 
+    renderUpdateProductForm() {
+        const singleProduct = this.props.product
+        return (
+            <div>
+                <Panel header={`Product # ${singleProduct.productId}`} bsStyle="info">
+                    <div>Item:
+                        <input
+                            type="text"
+                            value={this.state.description}
+                            onChange={this.updateSearch} />
+                    </div>
+                    <div>Quantity:
+                        <input
+                            type="text"
+                            value={this.state.quantity}
+                            onChange={this.updateSearch} />
+                    </div>
+                    <div>Price:
+                        <input
+                            type="text"
+                            value={this.state.price}
+                            onChange={this.updateSearch} />
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-info btn-xs">
+                        <span className="glyphicon glyphicon-plus" /> Submit Change
+                    </button>
+                </Panel>
+            </div >
+        )
     }
 }
 
 const mapStateToProps = (state) => {
-    console.log('STATE', state);
     return {
         product: state.products[0],
     }
 }
 
-export default connect(mapStateToProps, { getSingleProduct, deleteProduct })(SingleProduct);
+export default connect(mapStateToProps, { getSingleProduct, deleteProduct, updateProduct })(SingleProduct);
