d91280b95ff50e611767593f549e9cad62c2c8dc
Alexander Zoitos <azoitos1@gmail.com>
2017-11-30 16:22:29 -0500

    Add: GET all test routes

--- server/start.js
+++ server/start.js
@@ -7,7 +7,7 @@ var debug = require('debug')('app')
 app.set('port', process.env.PORT || 3000);
 
 //sync database then start up server
-db.sync()
+db.sync({force: true})
     .then(function () {
         var server = app.listen(app.get('port'), function () {
             debug('Express server listening on port ' + server.address().port)
--- tests/route.test.js
+++ tests/route.test.js
@@ -39,5 +39,58 @@ describe('Products Route', () => {
                     expect(res.body).to.have.length(0);
                 });
         })
+
+        it('returns a product if it is in the DB', function() {
+            var product = Product.create({
+                productId: 1,
+                description: 'This is a test product',
+                quantity: 42,
+                price: 2017
+            });
+
+            return product
+            .then( () => {
+                return agent
+                .get('/api/products')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).to.be.an.instanceof(Array);
+                    expect(res.body[0].description).to.equal('This is a test product');
+                    expect(res.body).to.have.length(1);
+                })
+            })
+        })
+
+        it('returns multiple products in the DB', function(){
+            var product1 = Product.create({
+                productId: 1234,
+                description: 'This is a test product',
+                quantity: 42,
+                price: 2017
+            });
+
+            var product2 = Product.create({
+                productId: 4567,
+                description: 'This is a test 2 product',
+                quantity: 23,
+                price: 1892
+            });
+
+            return product1
+            .then(() => {
+                return product2
+            })
+            .then(() => {
+                return agent
+                .get('/api/products')
+                .expect(200)
+                .expect(res => {
+                    expect(res.body).to.be.an.instanceof(Array)
+                    expect(res.body[0].productId).to.equal(1234)
+                    expect(res.body[1].quantity).to.equal(23)
+                    expect(res.body).to.have.length(2);
+                })
+            })
+        })
     })
 })
