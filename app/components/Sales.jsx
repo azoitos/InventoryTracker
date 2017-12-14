2821384750dc0cb88e5b45249c3cba2310dea4e7
Alexander Zoitos <azoitos1@gmail.com>
2017-12-14 14:54:05 -0500

    Fix: Sales Report Format

--- app/components/Sales.jsx
+++ app/components/Sales.jsx
@@ -1,67 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { LineChart } from 'react-d3-basic';
+import { Table } from 'react-bootstrap';
+// import { LineChart } from 'react-d3-basic';
 
 import { getAllSales } from '../reducers/salesReport.jsx'
-
-let width = 700,
-    height = 300,
-    margins = { left: 100, right: 100, top: 50, bottom: 50 },
-    title = "Sales Made",
-    // chart series,
-    // field: is what field your data want to be selected
-    // name: the name of the field that display in legend
-    // color: what color is the line
-    chartSeries = [
-        {
-            field: 'id',
-            name: 'Sales Made',
-            color: '#ff7f0e'
-        }
-    ],
-    // your x accessor
-    x = function (d) {
-        return d.id;
-    },
-    chartData = [
-        {
-            name: "Lavon Hilll I",
-            BMI: 20.57,
-            age: 12,
-            birthday: "1994-10-26T00:00:00.000Z",
-            city: "Annatown",
-            married: true,
-            index: 1
-        },
-        {
-            name: "Clovis Pagac",
-            BMI: 24.28,
-            age: 26,
-            birthday: "1995-11-10T00:00:00.000Z",
-            city: "South Eldredtown",
-            married: false,
-            index: 3
-        },
-        {
-            name: "Gaylord Paucek",
-            BMI: 24.41,
-            age: 30,
-            birthday: "1975-06-12T00:00:00.000Z",
-            city: "Koeppchester",
-            married: true,
-            index: 5
-        },
-        {
-            name: "Ashlynn Kuhn MD",
-            BMI: 23.77,
-            age: 32,
-            birthday: "1985-08-09T00:00:00.000Z",
-            city: "West Josiemouth",
-            married: false,
-            index: 6
-        }
-    ]
-
 
 class Sales extends Component {
     constructor(props) {
@@ -76,16 +18,36 @@ class Sales extends Component {
         const { sales } = this.props;
         return (
             <div>
-                <LineChart
-                    showXGrid={false}
-                    showYGrid={false}
-                    title={title}
-                    margins={margins}
-                    data={sales}
-                    width={width}
-                    height={height}
-                    chartSeries={chartSeries}
-                    x={x} />
+                <h1>SALES REPORT</h1>
+                <Table responsive>
+                    <thead>
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Description</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {sales.length ? sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td>{sale.product.productId}</td>
+                                    <td>{sale.product.description}</td>
+                                    <td>{sale.product.quantity}</td>
+                                    <td>{sale.product.price}</td>
+                                </tr>
+                            )
+                        }) :
+                            <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+                        }
+                    </tbody>
+                </Table>
             </div>
         )
     }
