256d9c16d94bc0320f70024c1507e0d5b0c9b732
Alexander Zoitos <azoitos1@gmail.com>
2017-12-27 13:29:04 -0500

    Add: Expense Report - frontEnd

--- app/components/Expenses.jsx
+++ app/components/Expenses.jsx
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Table } from 'react-bootstrap';
+import { getAllExpenses } from '../reducers/expenseReport.jsx';
 
-const Expense = () => {
-    return (
-        <div>Expense Report coming soon...</div>
-    )
+
+class Expenses extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.getAllExpenses();
+    }
+
+    render() {
+        const { expenses } = this.props
+        return (
+            <div>
+                <h1>EXPENSE REPORT</h1>
+                <Table responsive>
+                    <thead>
+                        <tr>
+                            <th>Expense Type</th>
+                            <th>Description</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {expenses.length ? expenses.map(expense => {
+                            return (
+                                <tr key={expense.id}>
+                                    <td>{expense.type}</td>
+                                    <td>{expense.description}</td>
+                                    <td>{expense.amount}</td>
+                                </tr>
+                            )
+                        }) :
+                            <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+                        }
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
 }
 
-export default Expense;
+function mapStateToProps(state) {
+    return {
+        expenses: state.expenses
+    }
+}
+
+export default connect(mapStateToProps, { getAllExpenses })(Expenses);
