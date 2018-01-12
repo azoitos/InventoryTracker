62b03c6967f7b89ee7ccb7f90dc3de9056ee65bf
Alexander Zoitos <azoitos1@gmail.com>
2018-01-12 10:28:48 -0500

    Refactor: Make Dropdown Button functional component

--- app/components/common/DropdownButton.js
+++ app/components/common/DropdownButton.js
@@ -1,35 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
-class selectDropDown extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            filter: 'productId'
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange(event){
-        this.setState({
-            filter: event.target.value
-        })
-    }
-
-    render() {
-        return (
-            <Form>
-                <FormGroup controlId="formControlsSelect">
-                    <ControlLabel>Filter Search By</ControlLabel>
-                    <FormControl className="dropdown" componentClass="select" onChange={this.props.onDropdownChange}>
-                        <option value="productId">ProductId</option>
-                        <option value="category">Category</option>
-                        <option value="description">Description</option>
-                    </FormControl>
-                </FormGroup>
-            </Form>
-        );
-    }
+function selectDropDown(props) {
+    return (
+        <Form>
+            <FormGroup controlId="formControlsSelect">
+                <ControlLabel>Filter Search By</ControlLabel>
+                <FormControl className="dropdown" componentClass="select" onChange={props.onDropdownChange}>
+                    <option value="productId">ProductId</option>
+                    <option value="category">Category</option>
+                    <option value="description">Description</option>
+                </FormControl>
+            </FormGroup>
+        </Form>
+    );
 }
+
 
 export default selectDropDown;
